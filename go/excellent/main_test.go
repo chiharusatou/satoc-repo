cc4673bb369eafbfbbbcd2debbda072a733a7a6b
chiharusatou <sato.chiharu@cloudcentric.co.jp>
2025-11-27 18:34:39 +0900

    feat: Add go test workflow and sample code2

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -6,7 +6,7 @@ import "testing" // 標準のテストライブラリをインポート
 // Goのテスト関数は、ファイル名が *_test.go であり、関数名が TestXxx の形式でなければなりません。
 func TestEvenOrOdd(t *testing.T) {
 	// 1. 関数の実行（テスト対象）
-	result := EvenOrOdd(10)
+	result := EvenOrOdd(12)
 
 	// 2. 結果の検証（アサーション）
 	// 期待される結果が "even" であるかを確認
